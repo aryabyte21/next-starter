76468e4552a3b37be0370062fdd0f15315a292b5
Arya <aryaabyte@gmail.com>
2025-05-14 14:20:45 +0800

    fix: linting

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import NavLinks from "@/app/ui/dashboard/nav-links";
 import AcmeLogo from "@/app/ui/acme-logo";
 import { PowerIcon } from "@heroicons/react/24/outline";
-import { signOut } from "@/auth";
 import { signOutAction } from "@/app/lib/actions";
 
 export default function SideNav() {
