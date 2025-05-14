c2afb516bc65439195851368d99a55c6629131a3
Arya <aryaabyte@gmail.com>
2025-05-14 14:18:20 +0800

    feat: Authentication and metadata ✅

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,10 @@ import { z } from "zod";
 import postgres from "postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+import { signOut } from "@/auth";
+
 export type State = {
   errors?: {
     customerId?: string[];
@@ -105,4 +109,27 @@ export async function deleteInvoice(id: string) {
   } catch (error) {
     console.log(error);
   }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+){
+  try{
+    await signIn('credentials', formData);
+  } catch(error){
+    if(error instanceof AuthError){
+      switch(error.type){
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.'
+      }
+    }
+    throw error;
+  }
+}
+
+export async function signOutAction() {
+  await signOut({ redirectTo: "/" });
 }
